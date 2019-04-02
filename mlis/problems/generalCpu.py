c24ec07c1b44c208439bf59a2c35dff781e75ecf
alorlov <sasha.bada.bum@gmail.com>
2019-04-02 23:15:30 +0400

    generalCpu solution

--- mlis/problems/generalCpu.py
+++ mlis/problems/generalCpu.py
@@ -8,26 +8,56 @@ import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from ..utils import solutionmanager as sm
+import sys
+sys.path.append('./../utils')
+import solutionmanager as sm
+from gridsearch import GridSearch
+import pandas as pd
+import numpy as np
 
+
+LOSSES = {
+        'MSELoss': nn.MSELoss(reduction='sum'),
+        'BCELoss': nn.BCELoss(),
+        }
+
+ACTIVATIONS_GRID = {
+        'sigmoid': nn.Sigmoid(),
+        'relu': nn.ReLU(),
+        'tanh': nn.Tanh(),
+        'hardshrink': nn.Hardshrink(),
+        'htang1': nn.Hardtanh(-1, 1),
+    }
 class SolutionModel(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size, params):
         super(SolutionModel, self).__init__()
+        self.params = params
         self.input_size = input_size
-        sm.SolutionManager.print_hint("Hint[1]: Explore more deep neural networks")
-        self.hidden_size = 10
-        self.linear1 = nn.Linear(input_size, self.hidden_size)
-        self.linear2 = nn.Linear(self.hidden_size, output_size)
+        
+        if params.grid_search.enabled:
+            torch.manual_seed(params.random)
 
+        sizes = (input_size, *params.hidden_sizes, output_size)
+        
+        self.layers = nn.ModuleList()
+        self.batch_norms = nn.ModuleList()
+        for idx in range(len(sizes) - 1):
+            linear = nn.Linear(sizes[idx], sizes[idx + 1])
+            if params.batch_norm:
+                bn = nn.BatchNorm1d(sizes[idx + 1], track_running_stats=False)
+            self.layers.append(linear)
+            self.batch_norms.append(bn)
+        
     def forward(self, x):
-        x = self.linear1(x)
-        x = torch.sigmoid(x)
-        x = self.linear2(x)
-        x = torch.sigmoid(x)
+        for idx, layer in enumerate(self.layers):
+            x = layer(x)
+            if idx != len(self.layers) - 1 and self.params.batch_norm:
+                x = self.batch_norms[idx](x)
+            x = ACTIVATIONS_GRID[self.params.activations[idx]](x)
         return x
 
     def calc_loss(self, output, target):
-        loss = ((output-target)**2).sum()
+        loss = LOSSES[self.params.loss](output, target)
         return loss
 
     def calc_predict(self, output):
@@ -36,47 +66,102 @@ class SolutionModel(nn.Module):
 
 class Solution():
     def __init__(self):
-        self = self
+        self.sols = {}
+        self.solsSum = {}
+
+        self.n_hidden = 5
+
+        self.hidden_sizes = [47] * self.n_hidden
+        self.activations = ['relu'] + (['relu'] * (self.n_hidden - 1)) + ['sigmoid']
+        self.loss = 'MSELoss'
+        self.lr = 0.00774
+        self.momentum = 0.91079
+        
+        self.random = 0
+        self.batch_norm = True
+        
+        self.hidden_sizes_grid = [
+            [i] * self.n_hidden
+            for i in [47]
+#            for i in np.linspace(3, 66, 40, dtype=int)
+
+        ]
+
+        self.activations_grid = [
+           [i] * self.n_hidden + [j]
+           for i in ['relu']
+           for j in ['sigmoid']
+        ]
+        
+        self.loss_grid = list(LOSSES)
+#        self.activation_grid = list(ACTIVATIONS_GRID)
+        self.lr_grid = np.round(np.geomspace(0.001, 10, 10), 5)
+#        self.hidden_size_grid = np.linspace(17, 35, 12, dtype=int)
+#        self.lr_grid = [0.001]
+#        self.lr_grid = np.linspace(0.001, 0.2, 11)
+#        1 - 10**(- np.random.rand(5) - 1)
+#        self.momentum_grid = np.round(np.geomspace(0.9, 0.99, 9), 5)
+        self.random_grid = range(3)
+        self.grid_search = GridSearch(self).set_enabled(False)
 
     def create_model(self, input_size, output_size):
-        return SolutionModel(input_size, output_size)
+        return SolutionModel(input_size, output_size, self)
+
+    def get_key(self):
+        return "{}_{}_{}_{}_{}".format(self.lr, self.momentum, self.hidden_sizes, self.activations, self.loss);
 
     # Return number of steps used
     def train_model(self, model, train_data, train_target, context):
+        # get off noise
+        key = self.get_key()
+        if not key in self.sols:
+            self.sols[key] = 0
+            self.solsSum[key] = 0
+        if self.sols[key] == -1:
+            return
+        
         step = 0
         # Put model in train mode
         model.train()
+#        optimizer = optim.SGD(model.parameters(), lr=self.lr, momentum=self.momentum)
+        optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        
         while True:
             time_left = context.get_timer().get_time_left()
-            # No more time left, stop training
-            if time_left < 0.1:
+            if time_left < 0.1 or (self.grid_search.enabled and step > 15):
+                self.sols[key] = -1
                 break
-            optimizer = optim.SGD(model.parameters(), lr=1.0)
             data = train_data
             target = train_target
             # model.parameters()...gradient set to zero
             optimizer.zero_grad()
             # evaluate model => model.forward(data)
-            sm.SolutionManager.print_hint("Hint[2]: Explore other activation functions", step)
             output = model(data)
             # if x < 0.5 predict 0 else predict 1
-            predict = model.calc_predict(output)
+            predict = output.round()
             # Number of correct predictions
             correct = predict.eq(target.view_as(predict)).long().sum().item()
             # Total number of needed predictions
-            total = predict.view(-1).size(0)
+            total = target.view(-1).size(0)
+            if correct == total:
+                self.sols[key] += 1
+                self.solsSum[key] += step
+                print("{:.4f} {}".format(step, key))
+                if self.sols[key] == len(self.random_grid):
+                    print('---confirmed {:.4f} ---'.format(float(self.solsSum[key])/self.sols[key]))
+                break
             # calculate loss
-            sm.SolutionManager.print_hint("Hint[3]: Explore other loss functions", step)
             loss = model.calc_loss(output, target)
+            self.grid_search.log_step_value('loss', loss.item(), step)
             # calculate deriviative of model.forward() and put it in model.parameters()...gradient
             loss.backward()
             # print progress of the learning
-            self.print_stats(step, loss, correct, total)
+#            self.print_stats(step, loss, correct, total)
             # update model: model.parameters() -= lr * gradient
             optimizer.step()
             step += 1
         return step
-    
+
     def print_stats(self, step, loss, correct, total):
         if step % 1000 == 0:
             print("Step = {} Prediction = {}/{} Error = {}".format(step, correct, total, loss.item()))
@@ -111,6 +196,9 @@ class DataProvider:
     def create_case_data(self, case):
         input_size = min(3+case, 7)
         data, target = self.create_data(input_size, case)
+#        x_norm = np.linalg.norm(data)
+#        data = data / x_norm
+#        print(data[:3])
         return sm.CaseData(case, Limits(), (data, target), (data, target)).set_description("{} inputs".format(input_size))
 
 
@@ -125,4 +213,7 @@ class Config:
         return Solution()
 
 # If you want to run specific case, put number here
+tic = time.time()
 sm.SolutionManager(Config()).run(case_number=-1)
+toc = time.time()
+print('proceed at %f s' %(toc - tic))
